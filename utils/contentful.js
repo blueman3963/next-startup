2c7b41f8fdd0ca270c7969f0ef980defdb4c3f8f
Blueman <yzhang01@mica.edu>
2019-09-09 21:18:45 -0400

    clean

--- utils/contentful.js
+++
@@ -1,8 +0,0 @@
-import * as contentful from 'contentful'
-
-const client = contentful.createClient({
-  space:'c65k38ppfuht',
-  accessToken:'dgFeLiRXkX0Ca6GMbYl1GNGnyB2OryTwdSrglxsSI5M'
-})
-
-export default client
